1ce127f54e96bd5b9c02c7dad4b741ccbe67beae
Platane <brongniart.arthur@gmail.com>
2016-02-29 23:06:18 +0100

    enlarge

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
 // swarm
 
-var rand = function(){ return 0|( Math.random()*500 ) }
+var rand = function(){ return 0|( Math.random()*640 ) }
 
 // init entities
-var l=250
+var l=280
 var n_team = 3
 entities = []
 for( var i=l;i--;)
-    entities[i]={x:rand(), y:rand(), target:3, vx:0, vy:0, color:rand()%n_team}
+    entities[i]={x:rand(), y:rand(), target:i%9, vx:0, vy:0, color:rand()%n_team}
+
 
 var honeyPot = [
-    {x:100, y:100},
-    {x:140, y:100},
-    {x:126, y:153},
-    {x:rand(), y:rand()},
-    {x:rand(), y:rand()},
-    {x:rand(), y:rand()},
-    {x:rand(), y:rand()},
-    {x:130, y:360},
+    {x:120,y:120},
+    {x:70,y:170},
+    {x:170,y:70},
 ]
+for( var k=3;k<9;)
+    honeyPot[k ++]={x:rand()+80, y:rand() +80}
 
-var x0 = 25
-var fatness = 10
-var friendlyness = 0.007
-var revulsion = 0.5
+var x0 = 45
+var fatness = 17
+var friendlyness = 0.009
+var revulsion = 1.6
 var fn = function( x ) {
     return  revulsion / ( x *x ) - friendlyness * Math.exp( -( x- x0 )*( x- x0 ) / ( fatness * fatness ) )
 }
 
 
 var pointers=[]
-d.onmousemove = function(e){
+a.onmousemove = function(e){
     pointers = [{x: e.pageX,y: e.pageY}]
 }
-d.ontouchmove= function(e){
+a.ontouchmove= function(e){
     pointers = []
     for( var i=e.touches.length; i--; )
         pointers.push({ x:e.touches[i].pageX, y:e.touches[i].pageY })
 }
-d.onclick=function(e){
+a.onclick=function(e){
     for(var i=honeyPot.length; i--;)
     {
         var OUx = honeyPot[ i ].x - e.pageX
@@ -78,13 +76,13 @@ var loop = function(){
 
                 if ( entities[j].color == entities[i].color ) {
 
-                    var k = Math.min( 0.5, fn( d ) )
+                    var k = Math.min( 0.9, fn( d ) )
                     friendshipx += k*OUx/d
                     friendshipy += k*OUy/d
 
                 } else {
 
-                    var k = Math.min( 0.5, 3 / ( d * d ) )
+                    var k = Math.min( 0.9, 5 / ( d * d ) )
                     ax += k*OUx/d
                     ay += k*OUy/d
                 }
@@ -129,17 +127,17 @@ var loop = function(){
         entities[i].vx = entities[i].vx * 0.92 +  ax * 0.1
         entities[i].vy = entities[i].vy * 0.92 +  ay * 0.1
 
-        entities[i].x += entities[i].vx * 18
-        entities[i].y += entities[i].vy * 18
+        entities[i].x += entities[i].vx * 22
+        entities[i].y += entities[i].vy * 22
 
-        if ( d < 26 )
+        if ( d < 36 )
             entities[i].target = entities[i].target >= n_team ? entities[i].color : rand()%(honeyPot.length-n_team)+n_team
     }
 
     //draw
 
 
-    c.clearRect(0,0,500,500)
+    c.clearRect(0,0,800,800)
 
     // draw tethering
     for( var i=l;i--;)
@@ -154,7 +152,7 @@ var loop = function(){
             var x = 1 - Math.abs( d - 30 ) / 5
 
             if ( x > 0 ) {
-                c.lineWidth = 0.5
+                c.lineWidth = 0.9
                 c.strokeStyle = 'hsla(' + ( entities[i].color*75 + 12 ) + ', 50%, 60%, '+ x +' )'
                 c.beginPath()
                 c.moveTo( entities[i].x, entities[i].y )
@@ -170,11 +168,12 @@ var loop = function(){
         c.beginPath()
         c.fillStyle= color
         c.strokeStyle= color
-        c.arc( entities[i].x, entities[i].y, 2, 0, 6.28 )
+        c.lineWidth = 1.7
+        c.arc( entities[i].x, entities[i].y, 2.8, 0, 6.28 )
         c.fill()
         c.beginPath()
         c.moveTo(entities[i].x, entities[i].y)
-        c.lineTo(entities[i].x - entities[i].vx * 50, entities[i].y - entities[i].vy * 50)
+        c.lineTo(entities[i].x - entities[i].vx * 60, entities[i].y - entities[i].vy * 60)
         c.stroke()
 
     }
@@ -183,7 +182,7 @@ var loop = function(){
     for( var i=honeyPot.length;i--;){
         c.strokeStyle= i >= n_team ? '#7356b1' : 'hsl(' + ( i*75 + 12 ) + ', 50%, 60%)'
         c.beginPath()
-        c.arc( honeyPot[i].x, honeyPot[i].y, 16, 0, 6.28 )
+        c.arc( honeyPot[i].x, honeyPot[i].y, 22, 0, 6.28 )
         c.stroke()
     }
 
--- shim.js
+++ shim.js
@@ -1,12 +1,15 @@
 
 
 var body=document.querySelector('body')
-body.setAttribute('style','margin:0')
+body.setAttribute('style','margin:0;display:flex;align-items:center;justify-content:center;')
+
+var MAX_WIDTH  = 800
+var MAX_HEIGHT = 800
 
 var canvas = document.createElement('canvas')
+canvas.width = Math.min( MAX_WIDTH, window.innerWidth )
+canvas.height= Math.min( MAX_HEIGHT, window.innerHeight )
 // canvas.setAttribute('style','width:100%;height:100%;')
-canvas.width=500
-canvas.height=500
 
 body.appendChild(canvas)
 

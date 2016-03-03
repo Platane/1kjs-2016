158a2a16efcc1378bcbce432560c6293d4cf1fe5
Platane <brongniart.arthur@gmail.com>
2016-03-03 01:33:21 +0100

    crush

--- crushed/index.js
+++ crushed/index.js
@@ -1,183 +1,149 @@
 // swarm
 m=Math
+z=m.sqrt
+b=m.min
 r = function(){ return 0|( m.random()*640 ) }
 
-l=180
+l=225
 e = []
 for(i=l;i--;)
-    e[i]={x:r(), y:r(), t:i%9, u:0, v:0, c:r()%3}
+    e[i]={x:r(), y:r(), t:i%9, u:0, v:0, c:r()%3 ,s:0}
 
 
 h = [
-    {x:120,y:120},
-    {x:70,y:170},
-    {x:170,y:70},
+    {x:120,y:120,s:0},
+    {x:60,y:200,s:0},
+    {x:200,y:60,s:0},
 ]
 for(k=3;k<9;)
     h[k ++]={x:r()+80, y:r() +80}
 
-var pointers=[]
+n=g=0
 a.onmousemove = function(e){
-    pointers = [{x: e.pageX,y: e.pageY}]
-}
-a.ontouchmove= function(e){
-    pointers = []
-    for( var i=e.touches.length; i--; )
-        pointers.push({ x:e.touches[i].pageX, y:e.touches[i].pageY })
-}
-a.onclick=function(e){
-    for(var i=h.length; i--;)
-    {
-        var OUx = h[ i ].x - e.pageX
-        var OUy = h[ i ].y - e.pageY
-
-        var d = Math.max( 0.1, OUx*OUx + OUy*OUy )
-
-        if ( d < 30 )
-            for( var k=e.length; k--;)
-                e[k].t = i
-    }
+    n=e.pageX
+    g=e.pageY
 }
 
 // loop
 u = function(){
 
+    c.clearRect(0,0,800,800)
+
+    //draw honey pot
+    for( i=9;i--;){
+        c.globalAlpha = c.lineWidth =1
+        c.strokeStyle= i > 2 ? '#aaa' : 'hsl(' + ( i*75 + 12 ) + ',50%,60%)'
+        c.beginPath()
+        c.arc( h[i].x, h[i].y, 22, 0, 6.3 )
+        c.stroke()
+        c.beginPath()
+        c.lineWidth = 4
+        c.arc( h[i].x, h[i].y, 24, 0, h[i].s/100 )
+        c.stroke()
+    }
+
     // fly
     for( i=l;i--;){
 
-        ax = 0
-        ay = 0
-
         o=e[i]
 
-        Ox = o.x
-        Oy = o.y
+        p = o.x
+        q = o.y
 
-        // for each other entity
-        fx = 0
-        fy = 0
+        v = w = s = t = 0
+
+
+        // draw
+        c.globalAlpha = c.lineWidth =1
+        c.strokeStyle= 'hsl(' + ( o.c*75 + 12 ) + ',50%,60%)'
+        c.beginPath()
+        c.arc( p, q, 2.8, 0, 6.3 )
+        c.moveTo(p, q)
+        c.lineTo(p - o.u * 60, q - o.v * 60)
+        c.stroke()
+
 
         for( j=l;j--;)
             if(i!=j){
 
-                x = Ox - e[j].x
-                y = Oy - e[j].y
+                x = p - e[j].x
+                y = q - e[j].y
 
-                d = Math.sqrt(x*x + y*y )
+                d = z(x*x + y*y )
 
 
                 if ( e[j].c == o.c ) {
 
-                    k = m.min( 1, 1.8 / ( d *d ) - 0.01 * m.exp( -( d- 45 )*( d- 45 ) / ( 17 * 17 ) ) )
-                    fx += k*x/d
-                    fy += k*y/d
+                    k = b( 1, 2 / ( d *d ) - 0.01 * m.exp( -( d- 45 )*( d- 45 ) / ( 17 * 17 ) ) )
+                    v += k*x/d
+                    w += k*y/d
+
+                    k = 1 - m.abs( d - 30 ) / 8
+
+                    if ( k > 0 && !((i+j) % 4) && i<j ) {
+                        c.globalAlpha = k
+                        c.lineWidth = 0.5
+                        c.moveTo( p, q )
+                        c.lineTo( e[j].x, e[j].y )
+                        c.stroke()
+                    }
 
                 } else {
 
-                    k = m.min( 1, 6 / ( d * d ) )
-                    ax += k*x/d
-                    ay += k*y/d
+                    k = b( 2, 10 / ( d * d ) )
+                    s += k*x/d
+                    t += k*y/d
                 }
             }
 
+
         // limit firendship influence
         // prevent from forming a firendship ball
-        j = m.sqrt( fx * fx + fy * fy )
-        j = j / m.min( j, 0.08 )
+        j = z( v * v + w * w )
+        j = j / b( j, 0.02 )
 
-        ax += fx / j
-        ay += fy / j
+        s += v / j
+        t += w / j
 
 
         // honey pot influence
 
-        x = h[ o.t ].x - Ox
-        y = h[ o.t ].y - Oy
+        x = h[ o.t ].x - p
+        y = h[ o.t ].y - q
 
-        d =m.sqrt(m.max( 0.1, x*x + y*y ))
+        d =z(m.max( 0.1, x*x + y*y ))
 
-        ax += 0.1*x/d
-        ay += 0.1*y/d
+        s += 0.1*x/d
+        t += 0.1*y/d
 
 
         // pointer influence
-        // for( var j = pointers.length; j--;) {
-        //     var OUx = Ox - pointers[j].x
-        //     var OUy = Oy - pointers[j].y
-        //
-        //
-        //     k = m.min( 0.5, 80 / ( h * h ) ) / m.sqrt( OUx*OUx + OUy*OUy )
-        //     ax += k*OUx
-        //     ay += k*OUy
-        // }
+        x = p - n
+        y = q - g
+
+        k = z( x*x + y*y )
+        k = b( 0.5, 80 / ( k*k ) ) / k
+        s += k*x
+        t += k*y
+
 
 
 
 
         // physic step
 
-        o.u = o.u * 0.92 +  ax * 0.1
-        o.v = o.v * 0.92 +  ay * 0.1
+        o.x += (o.u = o.u * 0.92 +  s * 0.1) * 22
+        o.y += (o.v = o.v * 0.92 +  t * 0.1) * 22
 
-        o.x += o.u * 22
-        o.y += o.v * 22
-
-        if ( d < 20 )
-            e[i].t = e[i].t >= 3 ? e[i].c : (r()%6)+3
-    }
-
-    //draw
-
-
-    c.clearRect(0,0,800,800)
-
-    // draw tethering
-    for( i=l;i--;)
-    for( j=i;j--;)
-        if(e[i].c == e[j].c && i+j % 6 ){
-
-            x = e[i].x - e[j].x
-            y = e[i].y - e[j].y
-
-            d = m.sqrt( x*x + y*y )
-
-            k = 1 - m.abs( d - 30 ) / 5
-
-            if ( x > 0 ) {
-                c.lineWidth = 0.9
-                c.strokeStyle = 'hsla(' + ( e[i].c*75 + 12 ) + ', 50%, 60%, '+ k +' )'
-                c.beginPath()
-                c.moveTo( e[i].x, e[i].y )
-                c.lineTo( e[j].x, e[j].y )
-                c.stroke()
-            }
-
+        if ( d < 18 ){
+            h[ o.t ].s ++
+            o.t = o.t >= 3 ? o.c : (r()%6)+3
         }
 
-    // draw e
-    for( i=l;i--;){
-        color = 'hsl(' + ( e[i].c*75 + 12 ) + ', 50%, 60%)'
-        c.strokeStyle= color
-        c.lineWidth = 1.7
-        c.beginPath()
-        c.arc( e[i].x, e[i].y, 2.8, 0, 6.28 )
-        c.moveTo(e[i].x, e[i].y)
-        c.lineTo(e[i].x - e[i].u * 60, e[i].y - e[i].v * 60)
-        c.stroke()
-
     }
-
-    //draw honey pot
-    for( i=h.length;i--;){
-        c.strokeStyle= i >= 3 ? '#7356b1' : 'hsl(' + ( i*75 + 12 ) + ', 50%, 60%)'
-        c.beginPath()
-        c.arc( h[i].x, h[i].y, 22, 0, 6.28 )
-        c.stroke()
-    }
-
-
 
     // loop
     requestAnimationFrame( u )
+    // setTimeout(u,16)
 }
 u()
